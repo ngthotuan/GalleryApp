f1167f5b026c6e649ebf630ed3da171190332800
nscon160498 <ncson1604@gmail.com>
2021-12-04 22:45:17 +0700

    take a picture

--- app/src/main/java/com/example/galleryapp/ui/camera/CameraFragment.java
+++ app/src/main/java/com/example/galleryapp/ui/camera/CameraFragment.java
@@ -1,26 +1,68 @@
 package com.example.galleryapp.ui.camera;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Camera;
+import android.graphics.Picture;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.example.galleryapp.R;
 import com.example.galleryapp.databinding.FragmentCameraBinding;
 
-public class CameraFragment extends Fragment {
-    FragmentCameraBinding binding;
 
+public class CameraFragment extends Fragment{
+    private FragmentCameraBinding binding;
+    private Button takeImg;
+    private ImageView imageView;
+    static final int CAPTURE_IMAGE_REQUEST=1;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentCameraBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
+        takeImg=binding.takePicture;
+        imageView = binding.imgCamera;
+        takeImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                captureImage();
+            }
+        });
         // do something here
-
         return root;
+    }
+    private void captureImage() {
+        if(ContextCompat.checkSelfPermission(takeImg.getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions((Activity) takeImg.getContext(),new String[] {Manifest.permission.CAMERA,Manifest.permission
+                    .WRITE_EXTERNAL_STORAGE},0);
+        }
+        else{
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(takePictureIntent,CAPTURE_IMAGE_REQUEST);
+        }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        Bundle extras = data.getExtras();
+        Bitmap imageBitmap = (Bitmap) extras.get("data");
+        imageView.setImageBitmap(imageBitmap);
     }
 
     @Override
@@ -28,4 +70,6 @@ public class CameraFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+
 }
