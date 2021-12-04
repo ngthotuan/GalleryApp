0e9e03a48044ef7f886c804ae3d2342684808b26
ngthotuan <ngthotuan@gmail.com>
2021-12-04 12:16:23 +0700

    sort pictures by name, date, size

--- app/src/main/java/com/example/galleryapp/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/galleryapp/ui/gallery/GalleryFragment.java
@@ -5,11 +5,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -26,6 +30,7 @@ import com.example.galleryapp.model.Picture;
 import com.example.galleryapp.utils.DateUtil;
 import com.example.galleryapp.utils.PictureUtil;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -38,8 +43,12 @@ public class GalleryFragment extends Fragment implements OnItemClick<Picture> {
     private List<Picture> pictures;
     private Button btnChangeView, btnSort;
     private EditText edtSearch;
+    private Spinner spSort;
     private boolean isGridView = true;
     private boolean sortDecreasing = true;
+    private List<String> sortFields;
+    private String sortField = "";
+    private String sortType = "desc";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -51,6 +60,7 @@ public class GalleryFragment extends Fragment implements OnItemClick<Picture> {
         btnChangeView = binding.btnChangeView;
         btnSort = binding.btnSort;
         edtSearch = binding.edtSearch;
+        spSort = binding.spSort;
 
         pictures = PictureUtil.getPictures(activity, null);
         adapter = getPictureAdapter(isGridView);
@@ -76,12 +86,11 @@ public class GalleryFragment extends Fragment implements OnItemClick<Picture> {
             @SuppressLint("NotifyDataSetChanged")
             @Override
             public void onClick(View view) {
-                Comparator<Picture> comparator = (p1, p2) -> sortDecreasing
-                        ? (int) (p1.getCreatedDate() - p2.getCreatedDate())
-                        : (int) -(p1.getCreatedDate() - p2.getCreatedDate());
+                Log.d("TAG", "sortField= " + sortField + " sortType= " + sortType);
+                Comparator<Picture> comparator = getComparator();
                 pictures.sort(comparator);
+                reversedSortType();
                 adapter.notifyDataSetChanged();
-                sortDecreasing = !sortDecreasing;
             }
         });
 
@@ -110,6 +119,22 @@ public class GalleryFragment extends Fragment implements OnItemClick<Picture> {
             }
         });
 
+        sortFields = getSortFields();
+        spSort.setAdapter(new ArrayAdapter<>(getContext(),
+                android.R.layout.simple_spinner_dropdown_item, sortFields));
+
+        spSort.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                sortField = sortFields.get(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
         return root;
     }
 
@@ -122,6 +147,39 @@ public class GalleryFragment extends Fragment implements OnItemClick<Picture> {
     private RecyclerView.LayoutManager getLayoutManger(boolean isGridView) {
         return isGridView ? new GridLayoutManager(getContext(), 4)
                 : new LinearLayoutManager(getContext());
+    }
+
+    private List<String> getSortFields() {
+        return Arrays.asList(
+                getResources().getString(R.string.name),
+                getResources().getString(R.string.size),
+                getResources().getString(R.string.created_date)
+        );
+    }
+
+    private Comparator<Picture> getComparator() {
+        Comparator<Picture> comparator;
+        if (sortField.equals(getResources().getString(R.string.size))) {
+            comparator = Comparator.comparing(Picture::getSize);
+        } else if (sortField.equals(getResources().getString(R.string.name))) {
+            comparator = Comparator.comparing(Picture::getName);
+        } else {
+            comparator = Comparator.comparing(Picture::getCreatedDate);
+        }
+
+        if (sortType.equals("desc")) {
+            comparator = comparator.reversed();
+        }
+        return comparator;
+    }
+
+    private void reversedSortType() {
+        if (sortType.equals("desc")) {
+            sortType = "asc";
+        } else {
+            sortType = "desc";
+        }
+        btnSort.setText(sortType);
     }
 
     @Override
