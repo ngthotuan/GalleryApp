bb3a05cf64ecbac96470478a899ab281e8daedee
tuan2012 <phampham1109@gmail.com>
2021-12-04 11:19:36 +0700

    save file photo

--- app/src/main/java/com/example/galleryapp/ui/album/PictureOfFolderFragment.java
+++ app/src/main/java/com/example/galleryapp/ui/album/PictureOfFolderFragment.java
@@ -1,15 +1,19 @@
 package com.example.galleryapp.ui.album;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,6 +35,9 @@ import com.example.galleryapp.model.Picture;
 import com.example.galleryapp.model.PictureFolder;
 import com.example.galleryapp.utils.PictureUtil;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -41,7 +48,8 @@ public class PictureOfFolderFragment extends Fragment implements OnItemClick<Pic
 
     private FragmentPictureOfFolderBinding binding;
     private final static int REQUEST_CODE_PHOTO = 50;
-
+    private PictureAdapter pictureAdapter;
+    ImageView imageView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -52,15 +60,18 @@ public class PictureOfFolderFragment extends Fragment implements OnItemClick<Pic
 
         RecyclerView imageRecycler;
         TextView txtTitle;
+
         Button btnTakePhoto;
         ProgressBar load;
+        imageView = binding.showingImage;
         btnTakePhoto = binding.btnTakePhoto;
         imageRecycler = binding.recycler;
         load = binding.loader;
         imageRecycler.hasFixedSize();
         load.setVisibility(View.VISIBLE);
         List<Picture> pictures = PictureUtil.getPictures(activity, pictureFolder.getPath());
-        imageRecycler.setAdapter(new PictureAdapter(R.layout.picture_item_gird, pictures, this));
+        pictureAdapter = new PictureAdapter(R.layout.picture_item_gird, pictures, this);
+        imageRecycler.setAdapter(pictureAdapter);
         imageRecycler.setLayoutManager(new GridLayoutManager(getContext(), 4));
 
 
@@ -70,6 +81,7 @@ public class PictureOfFolderFragment extends Fragment implements OnItemClick<Pic
             @Override
             public void onClick(View view) {
                 openSomeActivityForResult();
+//                onLaunchCamera(view);
             }
         });
 
@@ -81,19 +93,54 @@ public class PictureOfFolderFragment extends Fragment implements OnItemClick<Pic
     ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
+                @SuppressLint("NotifyDataSetChanged")
                 @Override
                 public void onActivityResult(ActivityResult result) {
                     if (result.getResultCode() == Activity.RESULT_OK) {
                         // There are no request codes
                         Intent data = result.getData();
                         Bitmap photo = (Bitmap) data.getExtras().get("data");
-                        Log.e("TAG", "onActivityResult: "+ photo );
+                        Log.e("TAG", "onActivityResult: " );
+//                        new ImageSave(getContext()).save(photo);
+                        imageView.setImageBitmap(photo);
+
+                        SaveFile(photo);
+                        pictureAdapter.notifyDataSetChanged();
                         ;
                     }
                 }
             });
 
 
+    public void SaveFile(Bitmap bitmap ) {
+        print("Creating cw");
+        ContextWrapper cw = new ContextWrapper(getContext());
+        print("Creating dir");
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        File file = new File(path, "DemoPicture.jpg");
+        print("path is" + path);
+
+        FileOutputStream fos = null;
+        try {
+            print("creating fos");
+            fos = new FileOutputStream(file);
+            print("Compressing bitmap");
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+                print("fos closed");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void print(String str){
+        Log.d("TAG", str);
+    }
     public void openSomeActivityForResult() {
         Intent [] intentArray;
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -110,8 +157,6 @@ public class PictureOfFolderFragment extends Fragment implements OnItemClick<Pic
         someActivityResultLauncher.launch(chooserIntent);
     }
 
-
-
     @Override
     public void onDestroyView() {
         super.onDestroyView();
@@ -122,4 +167,7 @@ public class PictureOfFolderFragment extends Fragment implements OnItemClick<Pic
     public void onClick(Picture item, int pos) {
         Toast.makeText(getContext(), item.toString(), Toast.LENGTH_SHORT).show();
     }
+
+
+
 }
