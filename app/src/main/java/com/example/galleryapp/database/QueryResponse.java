4eea222c84200b84b9d150a94cb4ce15c1f13029
caokimnhat <zjackanderson@gmail.com>
2021-12-09 08:45:24 +0700

    queryResponse use guide

--- app/src/main/java/com/example/galleryapp/database/QueryResponse.java
+++ app/src/main/java/com/example/galleryapp/database/QueryResponse.java
@@ -1,6 +1,29 @@
 package com.example.galleryapp.database;
 
+import com.example.galleryapp.model.Picture;
+
+import java.util.List;
+
 public interface QueryResponse<T> {
     void onSuccess(T data);
     void onFailure(String message);
 }
+
+/* GUIDE: example
+   public void example() {
+        int album_id = 2;
+        QueryContract.LinkQuery query = new LinkQueryImplementation();
+        query.getAllPictureInAlbum(2, new QueryResponse<List<Picture>>() {
+            @Override
+            public void onSuccess(List<Picture> data) {
+                // data is a list picture get from DB
+                // do something with it
+            }
+
+            @Override
+            public void onFailure(String message) {
+
+            }
+    });
+   }
+ */
