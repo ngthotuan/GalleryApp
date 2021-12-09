a6291739a95d6ce48aee8501ed8acf0c246c0de6
caokimnhat <zjackanderson@gmail.com>
2021-12-09 20:52:52 +0700

    refractor

--- app/src/main/java/com/example/galleryapp/database/databaseUtil/Config.java
+++ app/src/main/java/com/example/galleryapp/database/databaseUtil/Config.java
@@ -26,5 +26,4 @@ public class Config {
     public static final String TABLE_LINK_ID = "_id";
     public static final String ALBUM_ID_FK = "album_id";
     public static final String IMAGE_ID_FK = "image_id";
-    public static final String IMAGE_SUB_CONSTRAINT = "image_sub_unique";
 }
